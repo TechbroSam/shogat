a54c04e75c41d87eab11376faada890d9315bd20
brotherSam <samuelobior@yahoo.co.uk>
2024-04-29 19:37:42 +0100

    margin commit

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-import Moralis from "moralis";
-import { SolNetwork } from "@moralisweb3/common-sol-utils";
+const Moralis = require("moralis").default;
+const { SolNetwork } = require("@moralisweb3/common-sol-utils");
 
 const runApp = async () => {
   await Moralis.start({
     apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjI4YzRjYzc5LWYyMDYtNDIwYy04ZmI4LThmYWQyZmY4NDhmYyIsIm9yZ0lkIjoiMzY4ODYyIiwidXNlcklkIjoiMzc5MDk4IiwidHlwZUlkIjoiNjhlNGE1YWUtNGRiYi00ZmM3LWFkZjYtZjVmNGExNjRkNzE1IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MDMwMTAzNTMsImV4cCI6NDg1ODc3MDM1M30.BDM922jyo3k6BVW6b8sQmsEsu9I1OC-aPfwVTcRfquY"
   });
 
-  const address = "BG5Dp9gU5WbkHEaz6y95apb5NVPw3jC17M4ro27sgSXP";
+  const address = "BMwhQ1HXuzA7ucY8NYHyVq2uwuG47GHj1dNP6xJdoPzD";
 
   const network = SolNetwork.MAINNET;
 
@@ -15,11 +15,7 @@ const runApp = async () => {
     network,
   });
 
-   // Update the DOM with the fetched price
-  document.getElementById("token-price").innerText = '$' + response.get("price");
+  console.log(response.toJSON());
 };
 
-// Run the runApp function when the DOM content is loaded
-document.addEventListener("DOMContentLoaded", () => {
-  runApp();
-});
+runApp();
