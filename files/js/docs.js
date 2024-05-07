c893568ced4f5240b61b9a4efe4f6bcab3aa281c
brotherSam <samuelobior@yahoo.co.uk>
2024-05-07 19:22:42 +0100

    inner cover final

--- files/js/docs.js
+++ files/js/docs.js
@@ -21,7 +21,7 @@ function addPage(page, book) {
 	var element = $('<div />', {});
 
 	if (book.turn('addPage', element, page)) {
-		if (page<33) {
+		if (page<9) {
 			element.html('<div class="gradient"></div><div class="loader"></div>');
 			loadPage(page);
 		}
